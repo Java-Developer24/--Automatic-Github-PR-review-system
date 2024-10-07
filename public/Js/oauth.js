5918a63497ea4a05e8f811b760f0a352b73a6b2a
Developer@2446 <105372372+Java-Developer24@users.noreply.github.com>
2024-10-07 18:30:09 +0530

    Update oauth.js

--- public/Js/oauth.js
+++ public/Js/oauth.js
@@ -2,7 +2,7 @@
 
 
 const GITHUB_CLIENT_ID = "Ov23liUc212oQckd6RDF"
-const redirectUri = "https://automatic-github-pr-review-system.vercel.app/auth/github/callback"; // Backend callback URL
+const redirectUri = "https://automatic-github-pr-review-system-zwnu.vercel.app/auth/github/callback"; // Backend callback URL
 
 document.getElementById('githubLoginButton').addEventListener('click', () => {
   // const githubOAuthUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo%20user&redirect_uri=${redirectUri}`;
